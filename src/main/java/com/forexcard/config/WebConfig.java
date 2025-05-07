c71366897ac944f895e8a2957cddb08060d10236
Rohith251 <nukalarohith26@gmail.com>
2025-05-07 09:49:37 +0530

    Update WebConfig.java

--- src/main/java/com/forexcard/config/WebConfig.java
+++ src/main/java/com/forexcard/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://15.168.134.45:5173")
+                .allowedOriginPatterns("http://15.168.134.45:5173","http://172.18.0.3:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
